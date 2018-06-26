5249fba3a6bea0eea842eb32d181ae4658a25ee0
jeffptton <39930443+jeffptton@users.noreply.github.com>
2018-06-26 18:56:22 -0500

    Add files via upload

---
+++ LauraCroft.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fifteen
+{
+    class LauraCroft : TombRaider
+    {
+
+
+        public string _gender { get; set; }
+
+        public LauraCroft(string name, string costume, int power, string ability) : base(name, costume, power, ability)
+        {
+            _name = name;
+            _costume = costume;
+            _power = power;
+            _ability = ability;
+            _gender = _gender;
+        }
+
+        public bool Attack(string isAttacking)
+        {
+            bool BadGuy = true;
+            if (isAttacking == "Fighting")
+            {
+                return false;
+            }
+            else if (isAttacking == "training")
+            {
+                return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
+
---
+++ TombRaider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fifteen
+{
+    class TombRaider
+    {
+    
+        public string _name { get; set; }
+        public string _costume { get; set; }
+        public int _power { get; set; }
+        public string _ability { get; set; }
+
+        public TombRaider(string name, string costume, int power, string ability)
+        {
+            _name = name;
+            _costume = costume;
+            _power = power;
+            _ability = ability;
+        }
+    }
+}
